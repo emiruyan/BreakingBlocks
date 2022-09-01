5a2b7ac74780a648995cc82a190d46446ca1a4e3
emiruyan <emir-uyn@hotmail.com>
2022-09-01 14:03:08 +0300

    Shooter Controller
    ShooterController script created. Shooter can now move within certain limits.

---
+++ BreakingBlocks/Assets/Scripts/ShooterController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterController : MonoBehaviour
+{
+    void Update()
+    {
+        //mousePos isminde Vector3 değişkeni oluşturduk. Mouse koordinatı için ScreenToWorldPoint kullandık.
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(
+            Input.mousePosition.x, //Mouse'umuz sadece x yönünde gideceği için bu yeterli oldu.
+            transform.position.y,
+            transform.position.z
+            //Mouse pozisyonunu aldık.
+        ));
+
+        mousePos.x = Mathf.Clamp(mousePos.x,-2.4f, 2.4f);
+        //MathfClamp ile Shooter'ın hareketini ekran içinde kalacak şekilde sınırladık.
+
+        transform.position = new Vector3(
+            mousePos.x, //Mouse pozisyonunu kullandık.
+            transform.position.y,
+            transform.position.z
+
+        );
+    }
+}
